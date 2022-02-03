a0132fffbe54fdcd9f54c39962b0f7546f0b36a6
millasheshun <90781811+millasheshun@users.noreply.github.com>
2022-02-03 05:33:53 -0800

    initial commit

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TheMegApp extends StatelessWidget {
+  const TheMegApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.black87,
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
